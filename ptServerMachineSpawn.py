5eb2a164b8ceed1345b7c3bee11b20c75b93f3dd
vajith <vishnu.ajith@intel.com>
2024-05-10 06:02:18 -0700

    Set up logger for ptServerMachineSpawn

--- ptServerMachineSpawn.py
+++ ptServerMachineSpawn.py
@@ -12,6 +12,15 @@ import json
 from threading import Thread
 import time
 
+from log_config import get_server_logger
+
+server_logger = get_server_logger()
+log_banner = """
+************************************
+            PT SERVER 
+************************************
+"""
+
 ### this script will spawn the pt server machine
 def background_task(interval_sec):
     # run forever
@@ -21,9 +30,9 @@ def background_task(interval_sec):
         time.sleep(interval_sec)
         #print(globalVariable.runName)
         for t in getAllAvailbeMachine():
-            print(t['machineId'],t['heartBeat'])
+            server_logger.info(f" Background Task : MachineID: {t['machineId']}, Heartbeat: {t['heartBeat']}")
             if t['heartBeat'] > 10:
-                print("machine on machineID :",t["machineId"]," will be marked killed")
+                server_logger.warning("Background Task : machine on machineID :",t["machineId"]," will be marked killed")
                 setMachineKilledInMachineTrackerTable(t['machineName'],t['workWeek'],t['projectName'])
                 env = config[project]["gtEnv"]
                 wash = config[project]["wash"]
@@ -34,12 +43,11 @@ def background_task(interval_sec):
                 machineMemory = config[project]["machineMemory"]
                 ptServerTcl = config[project]["ptServerTcl"]
                 cmd = "nbjob run --target "+target+" --qslot "+qslot+" --wash  --class \'"+clas+""+machineMemory+"G&&"+Cores+"C\' xterm -T \""+t["machineName"]+"\" -e \"source setup"+t['machineName']+".csh\""
-                print ("run the command ", cmd)
+                server_logger.info(f"running {cmd}")
                 os.system(cmd)
 
-
             else:
-                print("increment the heartbeat by 1")
+                server_logger.info("Background Task: incrementing heartbeat of MachineID {t[machineId]} by 1")
                 hbeat = int(t['heartBeat']) + 1
                 updateMachineHeartbeat(t['machineId'],hbeat)
         # perform the task
@@ -111,16 +119,25 @@ config.read(globalVariable.configFile)
 machineSetup = config[project]["machineSetup"]
 count = 0
 
+server_logger.info(log_banner)
+server_logger.info(f"Current Project: {globalVariable.project}")
+server_logger.info(f"Current Block: {blockName}")
+server_logger.info(f"Current run name: {runName}")
+server_logger.info(f"Machine setup: {machineSetup}")
+
 setupDatabase()
 ## removed the fork task
+server_logger.info("Starting background tasks thread")
 daemon = Thread(target=background_task, args=(60,), daemon=True, name='Background')
 daemon.start()
 #daemon = Thread(target=machineSpawn_task, args=(60,), daemon=True, name='SpawnBackground')
 #daemon.start()
 
 if not args.restore:
+    server_logger.info("NOT A RESTORE SESSION. STARTING NEW PT SHELLS")
+    server_logger.info("Invoking PT Shells")
     for cornerMachine in machineSetup.split("\n"):
-        print("the number of machines that we need for corner ",cornerMachine.split(":")[0]," is ",cornerMachine.split(":")[1])
+        server_logger.info("the number of machines that we need for corner ",cornerMachine.split(":")[0]," is ",cornerMachine.split(":")[1])
         for cornerName in range(int(cornerMachine.split(":")[1])):
             corner = cornerMachine.split(":")[0]
             env = config[project]["gtEnv"]
@@ -132,6 +149,8 @@ if not args.restore:
             machineMemory = config[project]["machineMemory"]
             ptServerTcl = config[project]["ptServerTcl"]
 
+
+            
             #print("corner ",corner," spanning machine ", cornerName, " env ", env)
             ex_file = open("./setup"+corner+"_"+str(cornerName)+".csh","w")
             ex_file2 = open("./ptRun"+corner+"_"+str(cornerName)+".tcl","w")
@@ -156,11 +175,17 @@ if not args.restore:
             ex_file2.close()
             #code.interact(local=locals())
             if (globalVariable.noPTdebug):
+                server_logger.info("no PT Debug set. All machine's status set to ready")
                 status = "ready"
             else:
                 status = "loading"
                 print("Running command : ",cmd)
-                os.system(cmd)
+                try:
+                    os.system(cmd)
+                except Exception as e:
+                    server_logger.error(f"Machine not spawned. Error running njob command {cmd} : \n {e}")
+                    sys.exit(0)
+                    
                 ######### insert to database
                 #machineTrackerTable = ["machineId","machineName","corner","status","load","commandId","workWeek"]
                 machineName = corner+"_"+str(cornerName)
@@ -172,7 +197,8 @@ if not args.restore:
                 mySql = (machineName,corner,status,load,commandId,workWeek,projectName,blockName)
                 print(mySql,":: rcg")
                 writeToMachineTrackerTable(mySql)
-
+                
+    server_logger.info("Invoked all PT Shells")
 
 
 flag = 1
@@ -189,17 +215,13 @@ while (flag):
             do nothing
     """
     ## get the database
-    #code.interact(local=locals())
     notServiced = getAllNotServicedJobs()
-    #code.interact(local=locals())
     for job in notServiced:
-        #code.interact(local=locals())
+
         commandId = job['commandId']
         jobCorner = job['corner']
-        #print("jobs not serviced ", job)
-        #jobCorner = job.split(",")[1].split(":")[1].lstrip()
         machine = getAllAvailbeMachineForCorner(jobCorner)
-        #code.interact(local=locals())
+        
         if str(machine) != "[]":
             machine = machine[0]
             machine = str(machine)
@@ -210,9 +232,8 @@ while (flag):
                 machineName = machine.split(",")[1].split(":")[1].lstrip()
                 updateMachineNameInCommandInputTable(machineName,commandId)
             else:
-                print("waiting for machines")
-            #code.interact(local=locals())
+                server_logger.info(f"Waiting for machines")
+            
         else:
-            print("PT shell is not yet available for servicing commands")
+            server_logger.info("PT shell is not yet available for servicing commands")
     time.sleep(5)
-    #flag = 0
